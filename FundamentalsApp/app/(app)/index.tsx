8efc1e5b99b6572652fba5fdbab86b160b5c4acb
_DevNote <rommelagasa102@gmail.com>
2025-10-25 14:22:30 +0800

    learning StackNavigation

--- FundamentalsApp/app/(app)/index.tsx
+++ FundamentalsApp/app/(app)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable} from "react-native";
 import { Link } from "expo-router";
 
 export default function Home() {
@@ -21,6 +21,25 @@ export default function Home() {
       <Link href="/register">Register</Link>
       <Link href="/forgot-password">Forgot Password</Link>
 
+
+      <Link href="/products/best-sellers/playstation" asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Playstation</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor:  "#0ea5e9",
+      padding: 12,
+      borderRadius: 6
+    },
+    buttonText: {
+      fontSize: 16,
+      color: "white"
+    }
+
+})
---
+++ StackNavigationApp/app/_layout.tsx
@@ -0,0 +1,52 @@
+import { Stack } from "expo-router";
+import { Pressable, Text, Image} from "react-native";
+
+
+function LogoTitle() {
+  return (
+    <Image 
+      style={{ width: 30, height: 30}}
+      source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+    />
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{
+      headerStyle: { backgroundColor: "#6a51ae"},
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold"},
+          headerRight: () => (
+            <Pressable onPress={
+              () => alert("Menu button pressed!")
+            }>
+              <Text style={{color: "#fff", fontSize: 18, margin: 20, fontWeight: "bold"}}>Menu</Text>
+            </Pressable>
+          ),
+          headerTitle: (props) => <LogoTitle />
+    }}>
+      <Stack.Screen name="index" options={
+        {
+          title: "Home",
+          // headerStyle: { backgroundColor: "#6a51ae"},
+          // headerTintColor: "#fff",
+          // headerTitleStyle: { fontWeight: "bold"},
+          // headerRight: () => (
+          //   <Pressable onPress={
+          //     () => alert("Menu button pressed!")
+          //   }>
+          //     <Text style={{color: "#fff", fontSize: 18, margin: 20, fontWeight: "bold"}}>Menu</Text>
+          //   </Pressable>
+          // ),
+          // headerTitle: (props) => <LogoTitle />
+        }
+      }/>
+      <Stack.Screen name="about" options={
+        {
+          title: "About",
+        }
+      }/>
+    </Stack>
+  );
+}
